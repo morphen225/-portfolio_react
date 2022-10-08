4f1a1d62bd7f867e9d5aa1c3594c5a9756fbaf37
morphen225 <morphen225@gmail.com>
2022-10-08 18:26:19 +0530

    Portfolio

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,13 +1,13 @@
 const projectsData = [
- {
-      title: 'Portfolio',
-      date: '2022',
-      github_url: '',
-      project_url: '',
-      background: 'This project showcases my skill set, projects and experience through a online website, built using react.js',
-      role: 'Front-end Developer',
-      tech_stack: ['React.js','ES6','SCSS']
-}
-];
-
-export { projectsData };
+  {
+       title: 'Portfolio',
+       date: '2022',
+       github_url: 'https://github.com/morphen225/-portfolio_react',
+       project_url: 'https://github.com/morphen225/-portfolio_react.git',
+       background: 'This project showcases my skill set, projects and experience through a online website, built using react.js',
+       role: 'Front-end Developer',
+       tech_stack: ['React.js','ES6','SCSS']
+ }
+ ];
+ 
+ export { projectsData };
